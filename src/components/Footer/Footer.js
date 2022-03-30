2143686315fa154c448875de3cf05f5a6f241b93
MohammadFaysal731 <www.faysal731@gmail.com>
2022-03-30 23:03:58 +0600

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p><small className='text-danger'>&copy;Copyright Protected</small></p>
+        </div>
+    );
+};
+
+export default Footer;
